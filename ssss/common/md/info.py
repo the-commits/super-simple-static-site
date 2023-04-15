517fa753462c4261165b7c49d62d9f93f7af2e3c
Magnus Åberg <himself@magnusaberg.me>
2023-04-15 13:29:22 +0200

    Add meta variables in content
    Signed-off-by: Magnus Åberg <himself@magnusaberg.me>

--- ssss/common/md/info.py
+++ ssss/common/md/info.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 import markdown
 
 
@@ -11,4 +12,14 @@ def variables(template):
         if isinstance(value, list) and len(value) == 1:
             returned_variables[key] = value[0]
 
+    returned_variables["content"] = handle_meta_variables(returned_variables, returned_variables["content"], md=md)
+
     return returned_variables
+
+
+def handle_meta_variables(meta_vars, content, md=markdown.Markdown(extensions=["meta"])):
+    from jinja2 import Environment
+
+    env = Environment()
+    template = env.from_string(content)
+    return md.convert(template.render(**meta_vars))
